b88236c3bf3ea7d3b5198b3b56f0a63c91cdfc5e
Erick <erick.cobos@hotmail.com>
2023-06-01 13:45:38 -0600

    done

--- deliveryApp/src/app/app-routing.module.ts
+++ deliveryApp/src/app/app-routing.module.ts
@@ -1,31 +1,55 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AutGuardGuard} from '../app/auth//guards/aut-guard.guard'
 
 const routes: Routes = [
   {
+    path: 'main',
+    loadChildren: () => import('./auth/main/main.module').then(m => m.MainPageModule)
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./protected-app/tabs/tabs.module').then( m => m.TabsPageModule),
+    
   },
   {
     path: 'detalle-negocio/:negocioId',
-    loadChildren: () => import('./detalle-negocio/detalle-negocio.module').then( m => m.DetalleNegocioPageModule)
+    loadChildren: () => import('./protected-app/detalle-negocio/detalle-negocio.module').then( m => m.DetalleNegocioPageModule),
+    canActivate: [AutGuardGuard],
+    canLoad: [AutGuardGuard]
   },
 
   {
     path: 'carritos/:usuarioId',
-    loadChildren: () => import('./carritos/carritos.module').then( m => m.CarritosPageModule)
+    loadChildren: () => import('./protected-app/carritos/carritos.module').then( m => m.CarritosPageModule),
+    canActivate: [AutGuardGuard],
+    canLoad: [AutGuardGuard]
   },
   {
     path: 'pedidos',
-    loadChildren: () => import('./pedidos/pedidos.module').then( m => m.PedidosPageModule)
+    loadChildren: () => import('./protected-app/pedidos/pedidos.module').then( m => m.PedidosPageModule),
+    canActivate: [AutGuardGuard],
+    canLoad: [AutGuardGuard]
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () => import('./protected-app/perfil/perfil.module').then( m => m.PerfilPageModule),
+    canActivate: [AutGuardGuard],
+    canLoad: [AutGuardGuard]
   },
   {
     path: 'detalles-perfil/:usuarioId',
-    loadChildren: () => import('./detalles-perfil/detalles-perfil.module').then( m => m.DetallesPerfilPageModule)
+    loadChildren: () => import('./protected-app/detalles-perfil/detalles-perfil.module').then( m => m.DetallesPerfilPageModule),
+    canActivate: [AutGuardGuard],
+    canLoad: [AutGuardGuard]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
   },
 ];
 @NgModule({
